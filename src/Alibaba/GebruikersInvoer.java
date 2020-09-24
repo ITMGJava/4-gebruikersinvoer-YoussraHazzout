3a8c1bccce3f96d73b261fb08a077e581b9eb720
YoussraHazzout <YoussraHazzout@gmail.com>
2020-09-24 12:31:41 +0200

    Alibaba1.1

--- src/Alibaba/GebruikersInvoer.java
+++ src/Alibaba/GebruikersInvoer.java
@@ -3,12 +3,12 @@ package Alibaba;
 import java.util.Scanner;
 
 public class GebruikersInvoer {
-public static void main(String[] invoer){
-new Scanner(System.in);
-Scanner s = new Scanner(System.in);
-System.out.println("Voer tekst in: ");
-String tekst = s.nextLine();
-System.out.println(tekst.length());
+    public static void main(String[] invoer) {
+        new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
+        System.out.println("Voer tekst in: ");
+        String tekst = s.nextLine();
+        System.out.println(tekst.length());
 
-}
+    }
 }
